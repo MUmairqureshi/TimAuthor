c15d68ff3374e67caf944e2283a1514251a39561
MUmairqureshi <umairqadeer586@gmail.com>
2024-01-29 05:14:37 -0800

    add

--- src/Screens/BookManagement/EditBook.jsx
+++ src/Screens/BookManagement/EditBook.jsx
@@ -235,7 +235,7 @@ export const EditBook = () => {
                 document.querySelector('.loaderBox').classList.add("d-none");
                 console.log(error)
             })
-    };
+    }
 
 
 
